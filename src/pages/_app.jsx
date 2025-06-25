3f3807680fe673372ea7f5b390b81bd4cec79fbc
Arijit <arijit123roy098@gmail.com>
2025-06-25 17:01:48 +0530

    auth updated code

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,19 +1,19 @@
-import {
-  ClerkProvider,
-  SignInButton,
-  SignedIn,
-  SignedOut,
-  UserButton
-} from '@clerk/nextjs'
-import '@/styles/globals.css'
-return (
-  <ClerkProvider>
-    <SignedOut>
-      <SignInButton />
-    </SignedOut>
-    <SignedIn>
-      <UserButton />
-    </SignedIn>
-    <Component {...pageProps} />
-  </ClerkProvider>
-)
+// import {
+//   ClerkProvider,
+//   SignInButton,
+//   SignedIn,
+//   SignedOut,
+//   UserButton
+// } from '@clerk/nextjs'
+// import '@/styles/globals.css'
+// return (
+//   <ClerkProvider>
+//     <SignedOut>
+//       <SignInButton />
+//     </SignedOut>
+//     <SignedIn>
+//       <UserButton />
+//     </SignedIn>
+//     <Component {...pageProps} />
+//   </ClerkProvider>
+// )
